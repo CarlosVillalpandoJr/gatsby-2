7bfbfe5dfa3ada8d69b07fc392917b828e8242a3
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-04-29 15:48:09 -0500

    added css module

---
+++ tutorial-part-two/src/pages/about-css-modules.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Container from "../components/container"
+
+export default () => (
+    <div>
+        <h1>Cock balls</h1>
+        <Container>
+            <h1>About CSS Modules</h1>
+            <p>CSS Modules are cool</p>
+        </Container>
+    </div>
+)
